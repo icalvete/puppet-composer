37fc3b89e824256a8df0ad2903dd6232f00b56a0
Israel Calvete <icalvete@gmail.com>
2017-08-11 12:54:09 +0200

    In order to avoid memory issues with composer.

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,12 +9,13 @@ class composer::install {
   }
 
   exec{ 'composer_install':
-    command     => "curl -sS https://getcomposer.org/installer | php",
+    command     => 'curl -sS https://getcomposer.org/installer | php  -d memory_limit=-1',
     user        => 'root',
     cwd         => $composer::install_dir,
     provider    => 'shell',
     require     => File['composer_install_dir'],
     environment => 'COMPOSER_HOME=/root',
+    unless      => "/usr/bin/test -f ${composer::install_dir}/composer.phar"
   }
 
   file { '/usr/local/bin/composer.phar':
